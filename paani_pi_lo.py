460324b879672e0f7a81bcfa0516f741f7f7f28c
Jayant (An Indian) <64407898+Jayant-saksham@users.noreply.github.com>
2020-07-01 12:50:04 +0530

    Update paani_pi_lo.py

--- paani_pi_lo.py
+++ paani_pi_lo.py
@@ -5,7 +5,7 @@ while True:
     notification.notify(
         title='Jayant bhai, Please drink water',
         message='Reminder message from Python\nPani pii lo Jayant bhai\nWater is very important for our body',
-        app_icon='D:\Python learning\Python basics\water.ico',  # e.g. 'C:\\icon_32x32.ico'
+        app_icon='D:\Your_folder\your_icon.ico',  # e.g. 'C:\\icon_32x32.ico'
         timeout=10,  # seconds
     )
     time.sleep(60*60)
