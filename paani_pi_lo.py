464c7e07e9b10fd6127d7747ee75a5fa9e0747dd
Jayant <Jayant2410@gmail.com>
2020-07-01 12:34:51 +0530

    Initial commit

---
+++ paani_pi_lo.py
@@ -0,0 +1,11 @@
+from plyer import notification
+import time
+
+while True:
+    notification.notify(
+        title='Jayant bhai, Please drink water',
+        message='Reminder message from Python\nPani pii lo Jayant bhai\nWater is very important for our body',
+        app_icon='D:\Python learning\Python basics\water.ico',  # e.g. 'C:\\icon_32x32.ico'
+        timeout=10,  # seconds
+    )
+    time.sleep(60*60)
